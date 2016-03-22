b6583e6e08b246fc230ad6642cf932e7b8d29ee8
Vladislav Cebotari <cebotari.vladislav@gmail.com>
2016-03-22 09:47:27 +0200

    First commit. Black page test libs

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php 
+
+
+    $PAGE_NAME = "DOINAC - Internet of Things";
+
+ ?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Import stylesheets at the top for better user performance -->
+  <link rel="stylesheet" type="text/css" href="lib/bootstrap-3.3.6-dist/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+    <?php 
+     echo "<title>$PAGE_NAME</title>";
+    ?>
+</head>
+<body>
+
+
+
+
+
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+
+  <div class="container-fluid">
+    
+  </div>
+</nav>
+
+
+
+
+
+
+
+<!-- Put the script at the bottom for better performance -->
+<script src="lib/jquery-2.2.2.js" type="text/javascript" charset="utf-8"></script>
+<script src="lib/bootstrap-3.3.6-dist/js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
+</body>
+</html>
